8636f6ec938afaaf8ea743f498614f64bfafd5e4
Alain Noel <alain.noel34@gmail.com>
2024-06-12 18:02:45 +0200

    add routes + button shadcn components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
+import { Routes, Route } from 'react-router-dom';
 import './globals.css';
+import SigninForm from './auth/forms/SigninForm';
+import Home from './_root/pages/Home';
+import SignupForm from './auth/forms/SignupForm';
+import AuthLayout from './auth/AuthLayout';
+import RootLayout from './_root/RootLayout';
 
 const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Welcome to LinkXO!
-    </h1>
+    <main className="flex h-screen">
+      <Routes>
+        {/* public routes */}
+        <Route element={<AuthLayout />}>
+          <Route path="/sign-in" element={<SigninForm />} />
+          <Route path="/sign-up" element={<SignupForm />} />
+        </Route>
+
+        {/* private routes */}
+        <Route element={<RootLayout />}>
+          <Route index element={<Home />}/>
+        </Route>
+      </Routes>
+    </main>
   )
 }
 
