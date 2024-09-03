987a04a681e524020cc1572c3b3b0d16855ebccb
Alain Noel <alain.noel34@gmail.com>
2024-09-03 18:43:35 +0200

    fix bugs

--- src/components/ui/shared/PostStats.tsx
+++ src/components/ui/shared/PostStats.tsx
@@ -23,7 +23,7 @@ const PostStats = ({ post, userId }: PostStatsProps) => {
   const { data: currentUser } = useGetCurrentUser();
 
 
-  const savedPostRecord = currentUser?.save.find((record: Models.Document) => record.$id === post.$id)
+  const savedPostRecord = currentUser?.save.find((record: Models.Document) => record.$id === post?.$id)
 
   useEffect(() => {
     setIsSaved(!!savedPostRecord)
